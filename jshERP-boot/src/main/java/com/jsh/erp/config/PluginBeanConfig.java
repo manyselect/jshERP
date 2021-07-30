8d790d676d24fff19d04f43016714e5fc0ff348a
zhangxiang17 <zhang_xiang_per@163.com>
2021-07-30 18:08:16 +0800

    fix badrequest 400

--- jshERP-boot/src/main/java/com/jsh/erp/config/PluginBeanConfig.java
+++ jshERP-boot/src/main/java/com/jsh/erp/config/PluginBeanConfig.java
@@ -3,6 +3,10 @@ package com.jsh.erp.config;
 import com.gitee.starblues.extension.mybatis.SpringBootMybatisExtension;
 import com.gitee.starblues.integration.application.AutoPluginApplication;
 import com.gitee.starblues.integration.application.PluginApplication;
+import org.apache.catalina.connector.Connector;
+import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -20,5 +24,16 @@ public class PluginBeanConfig {
         PluginApplication pluginApplication = new AutoPluginApplication();
         pluginApplication.addExtension(new SpringBootMybatisExtension());
         return pluginApplication;
+    }
+    @Bean
+    public ConfigurableServletWebServerFactory webServerFactory() {
+        TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
+        factory.addConnectorCustomizers(new TomcatConnectorCustomizer() {
+            @Override
+            public void customize(Connector connector) {
+                connector.setProperty("relaxedQueryChars", "|{}[]");
+            }
+        });
+        return factory;
     }
 }
